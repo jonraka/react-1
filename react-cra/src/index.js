8a1554e4961e910d912150f76b8e9c77ee2bfb8b
jonraka <projectxpath@gmail.com>
2021-10-22 12:10:33 +0300

    react-cra

--- react-cra/src/index.js
+++ react-cra/src/index.js
@@ -1,8 +1,16 @@
+import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>hello world</h2>
+      </header>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +18,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
