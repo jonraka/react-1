5c2aae8039fa9e2d38813de772e5f19c654062e3
jonraka <projectxpath@gmail.com>
2021-10-22 12:15:01 +0300

    folders

---
+++ react-cra/src/components/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>hello world</h2>
+      </header>
+    </div>
+  );
+}
--- react-cra/src/index.js
+++ react-cra/src/index.js
@@ -1,16 +1,6 @@
-import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>hello world</h2>
-      </header>
-    </div>
-  );
-}
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
